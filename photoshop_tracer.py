1c3a54f193a8aca297a6febab58ec9afb1093143
ThePigrammer <thepigrammer@gmail.com>
2025-07-24 16:30:22 -0500

    initial commit

---
+++ photoshop_tracer.py
@@ -0,0 +1,24 @@
+import pyautogui
+
+def checkRow(x, y):
+    if pyautogui.pixelMatchesColor(x+1, y, (0, 0, 0), tolerance=10):
+        pyautogui.mouseDown()
+        x += 1
+        pyautogui.click(x, y)
+        checkRow(x, y)
+    
+
+pyautogui.click(pyautogui.locateCenterOnScreen("photoshop_icon.png"))
+
+x = 82
+y = 120
+
+for y in range(y, 1159, 3):
+    for x in range(x, 2209, 100):
+        pyautogui.moveTo(x,y)
+        if pyautogui.pixelMatchesColor(x, y, (0, 0, 0), tolerance=10):
+            pyautogui.click()
+            checkRow(x, y)
+        else:
+
+    x = 82
