9ea6108a77b8191ee0972a163be47edbbf76426d
ThePigrammer <thepigrammer@gmail.com>
2025-07-24 20:14:08 -0500

    changed from clicks to diagonal drags

--- photoshop_tracer.py
+++ photoshop_tracer.py
@@ -1,22 +1,52 @@
 import pyautogui
 
-def checkRow(x, y):
-    if pyautogui.pixelMatchesColor(x+1, y, (0, 0, 0), tolerance=10):
+def checkDiagonals(x, y):
+    fourSpots = []
+    tempX = x
+    tempY = y
+    while pyautogui.pixelMatchesColor(tempX, tempY, (0, 0, 0), tolerance=10):
+        tempX -= 5
+        tempY -= 5
+        pyautogui.moveTo(tempX, tempY)
+    fourSpots.append([tempX, tempY])
+    tempX = x
+    tempY = y
+    while pyautogui.pixelMatchesColor(tempX, tempY, (0, 0, 0), tolerance=10):
+        tempX += 5
+        tempY -= 5
+        pyautogui.moveTo(tempX, tempY)
+    fourSpots.append([tempX, tempY])
+    tempX = x
+    tempY = y
+    while pyautogui.pixelMatchesColor(tempX, tempY, (0, 0, 0), tolerance=10):
+        tempX -= 5
+        tempY += 5
+        pyautogui.moveTo(tempX, tempY)
+    fourSpots.append([tempX, tempY])
+    tempX = x
+    tempY = y
+    while pyautogui.pixelMatchesColor(tempX, tempY, (0, 0, 0), tolerance=10):
+        tempX += 5
+        tempY += 5
+        pyautogui.moveTo(tempX, tempY)
+    fourSpots.append([tempX, tempY])
+    tempX = x
+    tempY = y
+    for spot in fourSpots:
+        pyautogui.moveTo(x, y)
         pyautogui.mouseDown()
-        x += 1
-        pyautogui.click(x, y)
-        checkRow(x, y)
-    
+        pyautogui.moveTo(spot[0], spot[1], 0.5)
+        pyautogui.mouseUp()
+
 
 pyautogui.click(pyautogui.locateCenterOnScreen("photoshop_icon.png"))
 
 x = 82
 y = 120
 
-for y in range(y, 1159, 3):
+for y in range(y, 1159, 10):
     for x in range(x, 2209, 100):
-        pyautogui.moveTo(x,y)
+        pyautogui.moveTo(x, y)
         if pyautogui.pixelMatchesColor(x, y, (0, 0, 0), tolerance=10):
-            pyautogui.click()
-            checkRow(x, y)
+            checkDiagonals(x, y)
     x = 82
