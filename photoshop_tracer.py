8227a3b5e09a8881133d412d375a219e8651f598
ThePigrammer <thepigrammer@gmail.com>
2025-07-25 20:26:32 -0500

    removed unneeded cursor movements so runs faster

--- photoshop_tracer.py
+++ photoshop_tracer.py
@@ -7,35 +7,31 @@ def checkDiagonals(x, y):
     while pyautogui.pixelMatchesColor(tempX, tempY, (0, 0, 0), tolerance=10):
         tempX -= 5
         tempY -= 5
-        pyautogui.moveTo(tempX, tempY)
     fourSpots.append([tempX, tempY])
     tempX = x
     tempY = y
     while pyautogui.pixelMatchesColor(tempX, tempY, (0, 0, 0), tolerance=10):
         tempX += 5
         tempY -= 5
-        pyautogui.moveTo(tempX, tempY)
     fourSpots.append([tempX, tempY])
     tempX = x
     tempY = y
     while pyautogui.pixelMatchesColor(tempX, tempY, (0, 0, 0), tolerance=10):
         tempX -= 5
         tempY += 5
-        pyautogui.moveTo(tempX, tempY)
     fourSpots.append([tempX, tempY])
     tempX = x
     tempY = y
     while pyautogui.pixelMatchesColor(tempX, tempY, (0, 0, 0), tolerance=10):
         tempX += 5
         tempY += 5
-        pyautogui.moveTo(tempX, tempY)
     fourSpots.append([tempX, tempY])
     tempX = x
     tempY = y
     for spot in fourSpots:
         pyautogui.moveTo(x, y)
         pyautogui.mouseDown()
-        pyautogui.moveTo(spot[0], spot[1], 0.5)
+        pyautogui.moveTo(spot[0], spot[1], 0.1)
         pyautogui.mouseUp()
 
 
@@ -44,9 +40,8 @@ pyautogui.click(pyautogui.locateCenterOnScreen("photoshop_icon.png"))
 x = 82
 y = 120
 
-for y in range(y, 1159, 10):
+for y in range(y, 1159, 20):
     for x in range(x, 2209, 100):
-        pyautogui.moveTo(x, y)
         if pyautogui.pixelMatchesColor(x, y, (0, 0, 0), tolerance=10):
             checkDiagonals(x, y)
     x = 82
